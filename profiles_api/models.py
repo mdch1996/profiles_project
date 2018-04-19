da72508e8e884b09ba918705b5f4b261f64ea34f
Mohammad Chalaki <mohammadchalaki@Mohammads-MacBook-Pro.local>
2018-04-19 10:24:25 +0430

    Enabled the django admin for profiles_api app.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -21,7 +21,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
 
-        user = self.create_user(self, email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
