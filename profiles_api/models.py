be4279db4d607840e73014d39a63cc037cf76c22
Mohammad Chalaki <mohammadchalaki@Mohammads-MacBook-Pro.local>
2018-04-20 12:45:10 +0430

    Added GET, POST, PUT, PATCH and DELETE functions to APIView.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -31,7 +31,6 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+
+class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+
+            an_apiview = [
+                "ba slalm",
+                "how are u?",
+                "whats your name?"
+            ]
+
+            return Response({'message': "hello world", 'an_apiview': an_apiview})
+
+    def post(self, request):
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            # message = 'Hello {0}'.format(name)
+            return Response({'message': name})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+
+        return Response({'methode': 'put'})
+
+    def patch(self, request, pk=None):
+
+        return Response({'methode': 'patch'})
+
+    def delete(self, request, pk=None):
+
+        return Response({'methode': 'delete'})
+
