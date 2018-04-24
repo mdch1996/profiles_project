bd3ce034ba33e6fd90fe25aa35a0e70a46b92b6b
Mohammad Chalaki <mdch1996@gmail.com>
2018-04-24 19:32:16 +0430

    Added model for storing profile feed item and added to django-admin.

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -4,3 +4,4 @@ from . import models
 
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -52,3 +52,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
+class ProfileFeedItem(models.Model):
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_code = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_code
